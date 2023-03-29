4ac4146b79d7673519a033323e208697866ee01f
sockulags <125367352+sockulags@users.noreply.github.com>
2023-03-29 14:33:39 +0200

    -

--- Tetris/Assets/Scripts/ActivePiece1.cs
+++ Tetris/Assets/Scripts/ActivePiece1.cs
@@ -15,78 +15,44 @@ public class ActivePiece1 : Blocks
     {
 
         Board.Clear(this);
-        //Board.Clear(Board.activePiece2);
-this.lockTime += Time.deltaTime;
-        if (this.isPlayer1)
+
+        this.lockTime += Time.deltaTime;
+
+
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-
-            if (Input.GetKeyDown(KeyCode.Q))
-            {
-                Rotate(-1);
-            }
-            else if (Input.GetKeyDown(KeyCode.E))
-                Rotate(1);
-
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                Move(Vector2Int.left);
-            }
-            else if (Input.GetKeyDown(KeyCode.D))
-            {
-                Move(Vector2Int.right);
-            }
-
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                Move(Vector2Int.down);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                HardDrop();
-            }
-            if (Time.time >= this.stepTime)
-            {
-                Step();
-            }
+            Rotate(-1);
         }
-        if (!this.isPlayer1)
+        else if (Input.GetKeyDown(KeyCode.E))
+            Rotate(1);
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-
-            if (Input.GetKeyDown(KeyCode.UpArrow))
-            {
-                Rotate(-1);
-            }
-            //else if (Input.GetKeyDown(KeyCode.Ri))
-            //    Rotate(1);
-
-            if (Input.GetKeyDown(KeyCode.LeftArrow))
-            {
-                Move(Vector2Int.left);
-            }
-            else if (Input.GetKeyDown(KeyCode.RightArrow))
-            {
-                Move(Vector2Int.right);
-            }
-
-            if (Input.GetKeyDown(KeyCode.DownArrow))
-            {
-                Move(Vector2Int.down);
-            }
-
-            if (Input.GetKeyDown(KeyCode.RightShift))
-            {
-                HardDrop();
-            }
-            if (Time.time >= this.stepTime)
-            {
-                Step();
-            }
+            Move(Vector2Int.left);
         }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Move(Vector2Int.right);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Move(Vector2Int.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HardDrop();
+        }
+        if (Time.time >= this.stepTime)
+        {
+            Step();
+        }
+
 
         Board.Set(this);
-       
-        
+
+
     }
 
 
